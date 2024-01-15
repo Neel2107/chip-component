7273eb9ea49d085e2b4614579a8769c6de8c7a77
Neel <neelpatel805804@gmail.com>
2024-01-16 01:22:18 +0530

    add: highligh on backspace

--- src/app/components/ChipComponent.jsx
+++ src/app/components/ChipComponent.jsx
@@ -1,22 +1,18 @@
-"use client"
-import Image from 'next/image';
-import React, { useEffect, useRef, useState } from 'react'
-import { usersData } from '../utility/data';
+"use client";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+import { usersData } from "../utility/data";
 
 const ChipComponent = () => {
-  
-
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [chips, setChips] = useState([]);
   const [filteredData, setFilteredData] = useState(usersData);
   const [highlightedChip, setHighlightedChip] = useState(null);
 
   const wrapperRef = useRef(null);
 
-
-
   const handleKeyDown = (e) => {
-    if (e.key === 'Backspace' && inputValue === '') {
+    if (e.key === "Backspace" && inputValue === "") {
       if (highlightedChip !== null) {
         // Delete the highlighted chip
         handleRemoveChip(highlightedChip);
@@ -28,33 +24,33 @@ const ChipComponent = () => {
     }
   };
 
-
-  
   const handleInputChange = (e) => {
     const value = e.target.value;
     setInputValue(value);
-    setFilteredData(usersData.filter(item => item.name.toLowerCase().includes(value.toLowerCase())));
+    setFilteredData(
+      usersData.filter((item) =>
+        item.name.toLowerCase().includes(value.toLowerCase())
+      )
+    );
   };
 
   const handleItemClick = (item) => {
     setChips([...chips, item]);
-    setFilteredData(filteredData.filter(dataItem => dataItem !== item));
+    setFilteredData(filteredData.filter((dataItem) => dataItem !== item));
   };
 
   const handleRemoveChip = (chip) => {
-    setChips(chips.filter(c => c !== chip));
+    setChips(chips.filter((c) => c !== chip));
     if (highlightedChip === chip) {
       setHighlightedChip(null);
     }
     setFilteredData([...filteredData, chip]);
   };
 
- 
-
   useEffect(() => {
     function handleClickOutside(event) {
       if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
-        setInputValue(''); // Clear the input field
+        setInputValue(""); // Clear the input field
         setFilteredData(usersData); // Reset the filtered data to include all users
       }
     }
@@ -67,38 +63,52 @@ const ChipComponent = () => {
     };
   }, [wrapperRef]);
 
-  
-  
-
   return (
-    <div className='flex flex-col justify-center items-center w-full h-screen'>
-    <div ref={wrapperRef} className="flex flex-wrap gap-2 border border-zinc-400 h-auto w-2/3 rounded-md shadow-lg p-3">
-      <div className="flex flex-wrap flex-grow">
-        {chips.map((chip, index) => (
-               <div key={index} className={`flex items-center border border-gray-300 rounded-full px-2 py-1 m-1 ${chip === highlightedChip ? 'bg-red-200' : 'bg-zinc-200/50'}`}>
-
-            <Image src={chip.icon} alt="user" height={25} width={25} className="w-8 h-8 mr-2" />
-            <div className="flex flex-col">
-              <span>{chip.name}</span>
-              <span className="text-sm text-gray-500">{chip.email}</span>
+    <div className="flex flex-col justify-center items-center w-full h-screen">
+      <div
+        ref={wrapperRef}
+        className="flex flex-wrap gap-2 border border-zinc-400 h-auto w-2/3 rounded-md shadow-lg p-3"
+      >
+        <div className="flex flex-wrap flex-grow">
+          {chips.map((chip, index) => (
+            <div
+              key={index}
+              className={`flex items-center border border-gray-300 rounded-full px-2 py-1 m-1 ${
+                chip === highlightedChip ? "bg-red-200" : "bg-zinc-200/50"
+              }`}
+            >
+              <Image
+                src={chip.icon}
+                alt="user"
+                height={25}
+                width={25}
+                className="w-8 h-8 mr-2"
+              />
+              <div className="flex flex-col">
+                <span>{chip.name}</span>
+                <span className="text-sm text-gray-500">{chip.email}</span>
+              </div>
+              <button className="px-2" onClick={() => handleRemoveChip(chip)}>
+                <Image
+                  src={"/close.svg"}
+                  width={25}
+                  height={25}
+                  alt="close-icon"
+                />
+              </button>
             </div>
-            <button className='px-2' onClick={() => handleRemoveChip(chip)}>
-              <Image src={"/close.svg"} width={25} height={25} alt='close-icon' />
-            </button>
-          </div>
-        ))}
-        
+          ))}
+
           <input
             type="text"
-            className='text-start focus:outline-none'
+            className="text-start focus:outline-none"
             value={inputValue}
             onChange={handleInputChange}
             onKeyDown={handleKeyDown}
-            style={{ flexGrow: 1, flexBasis: '120px' }} // Ensuring the input has a base width and can grow
+            style={{ flexGrow: 1, flexBasis: "120px" }} // Ensuring the input has a base width and can grow
           />
-        
-      </div>
-      {inputValue && filteredData.length > 0 && (
+        </div>
+        {inputValue && filteredData.length > 0 && (
           <div className="absolute mt-12 w-2/3  bg-white border border-gray-300 rounded-md shdow-lg">
             {filteredData.map((item, index) => (
               <div
@@ -107,7 +117,13 @@ const ChipComponent = () => {
                 onClick={() => handleItemClick(item)}
               >
                 <div className="flex items-center">
-                  <Image src={item.icon} alt="user" height={25} width={25} className="w-8 h-8 mr-3" />
+                  <Image
+                    src={item.icon}
+                    alt="user"
+                    height={25}
+                    width={25}
+                    className="w-8 h-8 mr-3"
+                  />
                   <div className="flex flex-col">
                     <span>{item.name}</span>
                     <span className="text-sm text-gray-500">{item.email}</span>
@@ -120,7 +136,6 @@ const ChipComponent = () => {
       </div>
     </div>
   );
-}
+};
 
-
-export default ChipComponent
+export default ChipComponent;
